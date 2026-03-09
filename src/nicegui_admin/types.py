8f4d5c0014c77e68161c117e8ed4c06ae208d226
Julius Koenig <info@bastelquartier.de>
2026-03-09 23:55:31 +0100

    feat: add ExportType enumeration for different export formats

--- src/nicegui_admin/types.py
+++ src/nicegui_admin/types.py
@@ -1,5 +1,14 @@
+from enum import Enum
 from typing import Literal, Callable, Any, Awaitable
 
 SyncFunction = Callable[..., Any]
 AsyncFunction = Callable[..., Awaitable[Any]]
 SyncOrAsyncFunction = SyncFunction | AsyncFunction
+
+class ExportType(str, Enum):
+    """Enumeration of string constants that represent different export types."""
+
+    CSV = "csv"
+    EXCEL = "excel"
+    PDF = "pdf"
+    PRINT = "print"
